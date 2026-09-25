0c9dcec18ede7bedce623b61ac198f727194c5df
Mikalai Repin <mr@bam-boo.eu>
2026-09-25 16:00:40 +0300

    feat: installed prettier and formatted files

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,10 @@ function App() {
         </div>
         <div>
           <h1>React + Vite + TypeScript</h1>
-          <p>Это учебный проект на React, созданный для практики работы с компонентами и Git.</p>
+          <p>
+            Это учебный проект на React, созданный для практики работы с
+            компонентами и Git.
+          </p>
           <p>
             Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
           </p>
